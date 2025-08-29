e1589b8b921676fe51f7bf26e71c6f2458fa36c2
AbbasLewa <abbasomerhassn@gmail.com>
2025-08-29 06:08:43 -0700

    updating

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-center gap-6 bg-blue-300 p-4'>
         <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
+        <Link href="/about">AboutMe</Link>
         <Link href="/contact">Contact</Link>
     </div>
   )
