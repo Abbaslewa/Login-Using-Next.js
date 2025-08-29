49e3a4ab3bd7baacf7844b98c308f76ee7876072
AbbasLewa <abbasomerhassn@gmail.com>
2025-08-29 06:07:46 -0700

    updating

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-center gap-6 bg-blue-300 p-4'>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+    </div>
+  )
+}
+export default Navbar 
