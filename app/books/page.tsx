050aa9242ed46dc8f04ff44a2074d08bde9b3e66
AbbasLewa <abbasomerhassn@gmail.com>
2025-09-18 09:03:24 +0300

    added

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -40,7 +40,8 @@ export default function Books() {
         </h1>
         <p className="text-lg font-bold text-gray-700">
           Explore our recommended books to boost your English grammar,
-          vocabulary, and confidence. 
+          vocabulary, and confidence.
+          and confidence. 
           
         </p>
       </div>
